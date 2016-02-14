da514dfac177e1c56cf9e3fea222d4e49e018ecb
Konstantin Knauf <konstantin.knauf@tngtech.com>
2016-02-14 14:49:16 +0100

    Order Json Strings parsed from zeromq events

--- src/main/java/EMDR_Client.java
+++ src/main/java/EMDR_Client.java
@@ -2,18 +2,22 @@
  * Created by kknauf on 31.01.16.
  */
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.zeromq.*; // https://github.com/zeromq/jzmq
-import org.json.simple.*; // http://code.google.com/p/json-simple/downloads/list
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.zeromq.*;
+import org.json.simple.*;
 import org.json.simple.parser.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.UnsupportedEncodingException;
+import java.util.*;
 import java.util.zip.*;
 
 public class EMDR_Client {
+
+    public static final String EVE_RELAY = "tcp://relay-us-central-1.eve-emdr.com:8050";
+    public static final String KAFKA_BROKERS= "localhost:9092";
+
+    private static KafkaProducer kafkaProducer;
 
     public static void main(String[] args) throws Exception {
 
@@ -21,45 +25,61 @@ public class EMDR_Client {
         ZMQ.Socket subscriber = context.socket(ZMQ.SUB);
 
         // Connect to the first publicly available relay.
-        subscriber.connect("tcp://relay-us-central-1.eve-emdr.com:8050");
-
-        // Disable filtering.
+        subscriber.connect(EVE_RELAY);
         subscriber.subscribe(new byte[0]);
+
+        createKafkaProducerFromProgramArgs(KAFKA_BROKERS);
 
         while (true) {
             try {
                 // Receive compressed raw market data.
-                byte[] receivedData = subscriber.recv(0);
-
-                // We build a large enough buffer to contain the decompressed data.
-                byte[] decompressed = new byte[receivedData.length * 16];
-
-                // Decompress the raw market data.
-                Inflater inflater = new Inflater();
-                inflater.setInput(receivedData);
-                int decompressedLength = inflater.inflate(decompressed);
-                inflater.end();
-
-                byte[] output = new byte[decompressedLength];
-                System.arraycopy(decompressed, 0, output, 0, decompressedLength);
-
-                // Transform data into JSON strings.
-                String market_json = new String(output, "UTF-8");
-                ObjectMapper mapper = new ObjectMapper();
-
-                // Un-serialize the JSON data.
-                JSONParser parser = new JSONParser();
-                JSONObject market_data = (JSONObject)parser.parse(market_json);
+                JSONObject market_data = getNextJsonFromSocket(subscriber);
 
                 List<String> orderEvents = getOrderJsonStrings(market_data);
 
-                System.out.println(orderEvents);
-
+                writeStringsToKafkaTopic(orderEvents, "orders");
 
             } catch (ZMQException ex) {
                 System.out.println("ZMQ Exception occurred : " + ex.getMessage());
             }
         }
+    }
+
+    private static void createKafkaProducerFromProgramArgs(String servers) {
+        Properties kafkaProps = new Properties();
+        kafkaProps.setProperty("bootstrap.servers", servers);
+        kafkaProps.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        kafkaProps.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        kafkaProducer = new KafkaProducer(kafkaProps);
+    }
+
+    private static JSONObject getNextJsonFromSocket(ZMQ.Socket subscriber) throws DataFormatException, UnsupportedEncodingException, ParseException {
+        byte[] receivedData = subscriber.recv(0);
+
+        // We build a large enough buffer to contain the decompressed data.
+        byte[] decompressed = new byte[receivedData.length * 16];
+
+        // Decompress the raw market data.
+        Inflater inflater = new Inflater();
+        inflater.setInput(receivedData);
+        int decompressedLength = inflater.inflate(decompressed);
+        inflater.end();
+
+        byte[] output = new byte[decompressedLength];
+        System.arraycopy(decompressed, 0, output, 0, decompressedLength);
+
+        String market_json = new String(output, "UTF-8");
+
+        // Un-serialize the JSON data.
+        JSONParser parser = new JSONParser();
+        return (JSONObject)parser.parse(market_json);
+    }
+
+    private static void writeStringsToKafkaTopic(List<String> strings, String topic) {
+        for(String string: strings) {
+            kafkaProducer.send(new ProducerRecord(topic, null,string));
+        }
+
     }
 
     private static List<String> getOrderJsonStrings(JSONObject market_data) {
