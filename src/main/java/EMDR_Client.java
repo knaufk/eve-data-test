4d1682d47eec3d5d65dddfeb2b442a249be0a23c
Konstantin Knauf <konstantin.knauf@tngtech.com>
2016-02-14 14:48:48 +0100

    Order Json Strings parsed from zeromq events

--- src/main/java/EMDR_Client.java
+++ src/main/java/EMDR_Client.java
@@ -6,6 +6,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.zeromq.*; // https://github.com/zeromq/jzmq
 import org.json.simple.*; // http://code.google.com/p/json-simple/downloads/list
 import org.json.simple.parser.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.zip.*;
 
 public class EMDR_Client {
@@ -40,20 +45,47 @@ public class EMDR_Client {
 
                 // Transform data into JSON strings.
                 String market_json = new String(output, "UTF-8");
-
                 ObjectMapper mapper = new ObjectMapper();
 
                 // Un-serialize the JSON data.
                 JSONParser parser = new JSONParser();
                 JSONObject market_data = (JSONObject)parser.parse(market_json);
-                Object json = mapper.readValue(market_json, Object.class);
-                // Dump the market data to console or, you know, do more fun things here.
-                System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json));
+
+                List<String> orderEvents = getOrderJsonStrings(market_data);
+
+                System.out.println(orderEvents);
+
 
             } catch (ZMQException ex) {
                 System.out.println("ZMQ Exception occurred : " + ex.getMessage());
             }
         }
+    }
+
+    private static List<String> getOrderJsonStrings(JSONObject market_data) {
+        JSONArray rowSets = (JSONArray)market_data.get("rowsets");
+        JSONArray columns = (JSONArray)market_data.get("columns");
+        List<String> orderEvents = new ArrayList<String>();
+        for (Object rowSet: rowSets) {
+            JSONObject rowsetJson = (JSONObject) rowSet;
+
+            Long regionId = (Long) rowsetJson.get("regionID");
+            Long typeId = (Long) rowsetJson.get("typeID");
+
+            JSONArray rows = (JSONArray) rowsetJson.get("rows");
+
+            for (Object row : rows) {
+                JSONArray rowJson = (JSONArray) row;
+                Map<String, Object> outputRow = new HashMap<String, Object>();
+                for (int i=0; i<rowJson.size(); i++) {
+                    outputRow.put(columns.get(i).toString(), rowJson.get(i));
+                }
+                outputRow.put("regionID", regionId.toString());
+                outputRow.put("typeID", typeId.toString());
+                orderEvents.add(JSONObject.toJSONString(outputRow));
+            }
+        }
+        return orderEvents;
     }
 }
 
